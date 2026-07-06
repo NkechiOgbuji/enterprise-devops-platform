67df1006c6387012cf18e5871d13e81acc26da24
nkechiogbuji <ogbujiqueeneth13@gmail.com>
2026-07-06 06:01:51 +0100

    Complete reusable EKS Terraform module

---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "cluster" {
+  name = "${var.project_name}-${var.environment}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "main" {
+  name     = "${var.project_name}-${var.environment}-eks"
+  role_arn = aws_iam_role.cluster.arn
+  version  = var.cluster_version
+
+  vpc_config {
+    subnet_ids              = var.private_subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_policy
+  ]
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-eks"
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
